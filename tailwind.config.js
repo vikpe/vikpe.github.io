dfb9cfb29f4bcec516ec9b0d5018548bd51ff930
Viktor Persson <viktor.persson@arcsin.se>
2022-08-09 22:54:49 +0200

    Refactor styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
       sans: "Roboto, Helvetica, Arial, sans-serif",
       mono: "monospace",
     },
-    extend: {}
+    extend: {
+      colors: {
+        vblue: "#000225",
+        vcyan: "#04bff1",
+        vmagenta: "#ed0d94",
+      }
+    }
   },
   plugins: [
     require("@tailwindcss/typography")
