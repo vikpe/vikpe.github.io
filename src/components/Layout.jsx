f544f0db05477756f0485f2245feb699062bad4f
Viktor Persson <viktor.persson@arcsin.se>
2023-02-25 20:39:09 +0100

    Update dependencies.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import "astro/jsx-runtime";
 import DefaultLayout from "../layouts/default.astro";
 
-export default (props) => ({ children }) => (
-  <DefaultLayout {...props}>{children}</DefaultLayout>
-);
-
+export default (props) =>
+  ({ children }) =>
+    <DefaultLayout {...props}>{children}</DefaultLayout>;
